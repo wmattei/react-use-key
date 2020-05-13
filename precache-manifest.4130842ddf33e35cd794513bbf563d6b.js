2819eea9fdf88643e5a1d5812c23149ad424aa86
wmattei <wagner.mattei@gmail.com>
2020-05-13 20:22:34 -0300

    Updates

---
+++ precache-manifest.4130842ddf33e35cd794513bbf563d6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df4b972ebb6693fddc2ab9ac5f271cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b10b3c0a4a04233e8358",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e884f5e277a9ab04d9a",
+    "url": "/static/js/2.729e9dcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.729e9dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10b3c0a4a04233e8358",
+    "url": "/static/js/main.8a1f168b.chunk.js"
+  },
+  {
+    "revision": "f34ebc60959e15187e10",
+    "url": "/static/js/runtime-main.b4f8998f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
