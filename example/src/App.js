59ff294e137e755670a505335cf57fe51c7f2e33
wmattei <wagner.mattei@gmail.com>
2020-05-13 21:43:41 -0300

    Add support for META key

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useKey from 'react-use-key'
+import './index.css'
 
-import { ExampleComponent } from 'react-use-key'
-import 'react-use-key/dist/index.css'
+function App() {
+  const changeDocument = (e) => {
+    let newText = `You pressed '${e.key}' `
+    if (e.ctrlKey) newText += 'with CTRL, '
+    if (e.altKey) newText += 'with ALT, '
+    if (e.shiftKey) newText += 'with SHIFT, '
+    if (e.metaKey) newText += 'with Meta, '
+    setText(newText)
+  }
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  //Simple use
+  useKey('a', changeDocument, null)
+
+  //More than 1 match expression:
+  useKey('a|1|4|j|g', changeDocument, null)
+
+  //Combinations
+  useKey('ctrl+shift+1', changeDocument, null)
+
+  //Complex combinations
+  useKey('ctrl+shift+f | p', changeDocument, null)
+  useKey('alt+KeyG', changeDocument, { matchStrategy: 'code' })
+  useKey('meta+d', changeDocument, { preventDefault: true })
+
+  const [text, setText] = useState('')
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <span className='App-link'>{text}</span>
+      </header>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,88 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import { useCallback, useEffect } from 'react'
 
-interface Props {
-  text: string
+export interface Config {
+  separator?: string
+  orSeparator?: string
+  matchStrategy?: 'key' | 'code'
+  preventDefault?: boolean
+  eventType?: 'keydown' | 'keyup' | string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export declare type KeyboardEventHandler = (e: KeyboardEvent) => any
+
+const _defaultConfig: Config = {
+  separator: '+',
+  orSeparator: '|',
+  matchStrategy: 'key', // key || code
+  preventDefault: false,
+  eventType: 'keydown'
+}
+
+const _specialKeys = {
+  CTRL: 'CTRL',
+  SHIFT: 'SHIFT',
+  ALT: 'ALT',
+  META: 'META'
+}
+
+function _extractRegularKeys(string: string): string[] {
+  return string
+    .replace(/ /g, '')
+    .split('+')
+    .filter((o) => !Object.values(_specialKeys).includes(o))
+}
+
+/**
+ *
+ * @param {String} key
+ * @param {Function} handler
+ * @param {Object} config - Hook config object
+ * @param {String} [config.separator] - Keys separator
+ * @param {String} [config.orSeparator] - Or separator
+ * @param {String} [config.matchStrategy] - Strategy for matching gived string. Default: key
+ * @param {String} [config.preventDefault] - Should it prevent the default key behaviour
+ * @param {String} [config.eventType] - Event type. Default: keydown
+ */
+export default function useKey(
+  key: string,
+  handler: KeyboardEventHandler,
+  config: Config
+): any {
+  config = { ..._defaultConfig, ...(config || {}) }
+  const memoHandler = useCallback(handler, [])
+
+  const internalHandler = (e: KeyboardEvent) => {
+    console.log(e)
+    const options = key
+      .replace(/ /g, '')
+      .toUpperCase()
+      .split(config.orSeparator as string)
+    const match = options.find((o) => {
+      const combination = o.split(config.separator as string)
+      const requireCtrl = combination.includes(_specialKeys.CTRL)
+      const requireShift = combination.includes(_specialKeys.SHIFT)
+      const requireAlt = combination.includes(_specialKeys.ALT)
+
+      const regularKeys = _extractRegularKeys(o)
+      return (
+        regularKeys.includes(e[config.matchStrategy as string].toUpperCase()) &&
+        requireCtrl === e.ctrlKey &&
+        requireShift === e.shiftKey &&
+        requireAlt === e.altKey
+      )
+    })
+    if (!match) {
+      return
+    }
+    if (config.preventDefault) {
+      e.preventDefault()
+    }
+    memoHandler(e)
+  }
+
+  useEffect(() => {
+    document.addEventListener(config.eventType as string, internalHandler)
+    return () =>
+      document.removeEventListener(config.eventType as string, internalHandler)
+  }, [memoHandler, key])
 }
